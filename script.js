4a79fc028fe6b7de6f2dff395ef6b26ba3f7864c
Marcin <kwiatekm4@gmail.com>
2022-01-28 15:25:44 +0100

    show an error if not all inputs are filled

--- script.js
+++ script.js
@@ -4,15 +4,25 @@ collectPackageButton.addEventListener("click", function () { goToForm() })
 
 function goToForm() {
     let mainInformation = document.getElementById('mainInformation')
-    mainInformation.innerHTML =  
-    `<div class='packageCollectionForm'>
-        <div class='formItem'>
-            <div class='inputSignature'>Numer telefonu</div>
-            <input type='tel' class='inputForm' ></input>
-        </div>    
-        <div class='formItem'>
-            <div class='inputSignature'>Kod odbioru</div>
-            <input type='text' class='inputForm' ></input>
-        </div>    
-    </div>`
+    if (mainInformation.innerHTML === '') {
+        mainInformation.innerHTML =
+            `<div class='packageCollectionForm'>
+                <div class='formItem'>
+                    <div class='inputSignature'>Numer telefonu</div>
+                    <input type='tel' class='inputForm' id='phoneNumberInput' ></input>
+                </div>    
+                <div class='formItem'>
+                    <div class='inputSignature'>Kod odbioru</div>
+                    <input type='text' class='inputForm' id='pickupCodeInput'></input>
+                </div>    
+          </div>`
+    }
+    else if(document.getElementById('phoneNumberInput').value.length===0){
+        let formError = document.getElementById('formError')
+        formError.innerHTML = 'Pole numer telefonu nie może być puste!'
+    }
+    else if(document.getElementById('pickupCodeInput').value.length===0){
+        let formError = document.getElementById('formError')
+        formError.innerHTML = 'Pole kod odbioru nie może być puste!'
+    }
 }
